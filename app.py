77d8bdc46e50f9e555ab5625e9406f1845c948f4
denn <denn@u.sogang.ac.kr>
2020-08-18 18:28:06 +0900

    ver3

---
+++ app.py
@@ -0,0 +1,93 @@
+# -*- coding:utf-8 -*-
+#!usr/bin env python
+
+import cv2
+import json
+from flask import Flask, render_template, Response, request
+from flask_socketio import SocketIO, emit
+
+# emurated camera
+from webcamvideostream import WebcamVideoStream
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@app.route('/')
+def home():
+	return render_template("home.html")
+
+@app.route('/webcam', methods=['POST', 'GET'])
+def webcam():
+	return render_template("webcam2.html") # webcam.html은 작동함 2는 실험중
+
+def generate(camera):
+	while True:
+		frame = camera.get_frame()
+		if frame:
+			yield(b'--frame\r\n'
+				  b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+		else:
+			print("No Frame")
+			pass
+
+@app.route('/video_feed', methods=['GET', 'POST'])
+def video_feed():
+#	for video_frame in generate(WebcamVideoStream()):
+#		socketio.emit('from_flask', {'data':video_frame}, namespace='/test')
+	return Response(generate(WebcamVideoStream()),
+					mimetype="multipart/x-mixed-replace; boundary=frame")
+
+@app.route('/save_record', methods=["POST", "GET"])
+def save_record():
+	if request.method == 'POST':
+		f = request.files['file']
+		f.save('upload/'+f.filename)
+		return 'success'
+
+@app.route('/upload')
+def render_file():
+	return render_template('upload.html')
+
+@app.route("/fileUpload", methods=['GET', 'POST'])
+def upload_file():
+	if request.method == 'POST':
+		f = request.files['file']
+		f.save('upload/'+f.filename)
+
+		s = request.form['radio_sex']
+		json_data = {"sex":s}
+
+		with open("upload/"+f.filename[:-4]+".json", "w") as json_file:
+			json.dump(json_data, json_file)
+	
+	# docker openpose와 관련된 shell파일 실행
+	# result와 관련된 변수 정의
+	# return render_tempate("result.html", ...)
+	return "Success"
+
+@app.route('/comment')
+def comment():
+	result_json="resource/result_002.json"
+
+	with open(result_json, 'r') as json_file:
+		data=json.load(json_file)
+
+	return render_template("comment.html", data=data)
+
+@app.route('/result')
+def result():
+	# To-Do: 스켈레톤 이미지 띄워보기
+	# 아 스켈레톤 그대로 할지 openpose 쓸지 결정해야함!! -> 그려줄게
+	result_json = "resource/result_002.json"
+	sex_json = "upload/28-1_001-C09_mpi.json"
+	
+	with open(result_json, 'r') as json_file:
+		data=json.load(json_file)
+
+	with open(sex_json, "r") as json_file:
+		sex=json.load(json_file)
+
+	return render_template('result2.html', data=data, sex=sex)
+
+if __name__=='__main__':
+	app.run(host="0.0.0.0", port=8000, debug=True, threaded=True)
---
+++ static/js/webcam.js
@@ -0,0 +1,163 @@
+/*
+*  Copyright (c) 2015 The WebRTC project authors. All Rights Reserved.
+*
+*  Use of this source code is governed by a BSD-style license
+*  that can be found in the LICENSE file in the root of the source
+*  tree.
+*/
+
+// This code is adapted from
+// https://rawgit.com/Miguelao/demos/master/mediarecorder.html
+
+'use strict';
+
+/* globals MediaRecorder */
+
+let mediaRecorder;
+let recordedBlobs;
+
+const errorMsgElement = document.querySelector('span#errorMsg');
+const recordedVideo = document.querySelector('video#recorded');
+const recordButton = document.querySelector('button#record');
+recordButton.addEventListener('click', () => {
+  if (recordButton.textContent === 'Start Recording') {
+    startRecording();
+  } else {
+    stopRecording();
+    recordButton.textContent = 'Start Recording';
+    playButton.disabled = false;
+    downloadButton.disabled = false;
+  }
+});
+
+const playButton = document.querySelector('button#play');
+playButton.addEventListener('click', () => {
+  const superBuffer = new Blob(recordedBlobs, {type: 'video/mp4'});
+  recordedVideo.src = null;
+  recordedVideo.srcObject = null;
+  recordedVideo.src = window.URL.createObjectURL(superBuffer);
+  recordedVideo.controls = true;
+  recordedVideo.play();
+});
+
+const downloadButton = document.querySelector('button#download');
+downloadButton.addEventListener('click', () => {
+/*  var data = {};
+  data["video"] = new Blob(recordedBlobs, {type: 'video/mp4'});
+  console.log("hello");
+
+$.ajax({
+	url: "/output",
+	type: "POST",
+	contentType: "application/json",
+	data: data,
+});
+*/
+  var form = new FormData();
+  var filename = "webcam.mp4";
+
+  const blob = new Blob(recordedBlobs, {type: 'video/mp4'});
+  
+  form.append('file', blob);
+  form.append('title', filename);
+
+$.ajax({
+	type: 'POST',
+	enctype: 'multipart/form-data',
+	url: '/save_record',
+	data: form,
+	contentType: false,
+	}).done(function(data){ console.log(data);});
+  /*
+  const blob = new Blob(recordedBlobs, {type: 'video/mp4'});
+  const url = window.URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.style.display = 'none';
+  a.href = url;
+  a.download = 'test.mp4';
+  document.body.appendChild(a);
+  a.click();
+  setTimeout(() => {
+    document.body.removeChild(a);
+    window.URL.revokeObjectURL(url);
+  }, 100);
+  */
+});
+
+function handleDataAvailable(event) {
+  console.log('handleDataAvailable', event);
+  if (event.data && event.data.size > 0) {
+    recordedBlobs.push(event.data);
+  }
+}
+
+function startRecording() {
+  recordedBlobs = [];
+  let options = {mimeType: 'video/webm;codecs=vp9,opus'};
+  if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+    console.error(`${options.mimeType} is not supported`);
+    options = {mimeType: 'video/webm;codecs=vp8,opus'};
+    if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+      console.error(`${options.mimeType} is not supported`);
+      options = {mimeType: 'video/webm'};
+      if (!MediaRecorder.isTypeSupported(options.mimeType)) {
+        console.error(`${options.mimeType} is not supported`);
+        options = {mimeType: ''};
+      }
+    }
+  }
+
+  try {
+    mediaRecorder = new MediaRecorder(window.stream, options);
+  } catch (e) {
+    console.error('Exception while creating MediaRecorder:', e);
+    errorMsgElement.innerHTML = `Exception while creating MediaRecorder: ${JSON.stringify(e)}`;
+    return;
+  }
+
+  console.log('Created MediaRecorder', mediaRecorder, 'with options', options);
+  recordButton.textContent = 'Stop Recording';
+  playButton.disabled = true;
+  downloadButton.disabled = true;
+  mediaRecorder.onstop = (event) => {
+    console.log('Recorder stopped: ', event);
+    console.log('Recorded Blobs: ', recordedBlobs);
+  };
+  mediaRecorder.ondataavailable = handleDataAvailable;
+  mediaRecorder.start();
+  console.log('MediaRecorder started', mediaRecorder);
+}
+
+function stopRecording() {
+  mediaRecorder.stop();
+}
+
+function handleSuccess(stream) {
+  recordButton.disabled = false;
+  console.log('getUserMedia() got stream:', stream);
+  window.stream = stream;
+
+  const gumVideo = document.querySelector('video#gum');
+  gumVideo.srcObject = stream;
+}
+
+async function init(constraints) {
+  try {
+	const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    handleSuccess(stream);
+  } catch (e) {
+    console.error('navigator.getUserMedia error:', e);
+    errorMsgElement.innerHTML = `navigator.getUserMedia error:${e.toString()}`;
+  }
+}
+
+document.querySelector('button#start').addEventListener('click', async () => {
+  const constraints = {
+    audio: false, 
+	video: {
+      width: 1280, height: 720
+    }
+  };
+  console.log('Using media constraints:', constraints);
+  await init(constraints);
+});
